b46144f3bdbff8d8b3f1bdd73f97222eb8afb47f
Alexandre Negrel <negrel.dev@protonmail.com>
2023-05-14 21:44:50 +0200

    add minimal parser and gtk builder

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,49 @@
-use gtk::prelude::*;
-use mlua::prelude::*;
+use env_logger::Env;
+use gtk::{prelude::*, Builder};
 
-/// Application state.
-struct State {
-    lua: Lua,
-}
+mod ui;
 
-impl State {
-    pub fn new() -> Self {
-        Self { lua: Lua::new() }
-    }
-}
+use mlua::Lua;
 
-fn activate(app: &gtk::Application, state: &'static State) {
-    println!("activated");
+fn activate(app: &gtk::Application) {
+    let lua = Lua::new();
+
+    let ui_filepath = "./examples/helloworld/neww.ui";
+
+    log::debug!("reading {ui_filepath:?}...");
+    let ui_file = std::fs::read_to_string(ui_filepath).expect("Failed to read UI file");
+    log::debug!("{ui_filepath:?} read.");
+
+    log::debug!("parsing {ui_filepath:?}...");
+    let ui = ui::parse(&ui_file).expect("Failed to parse UI file");
+    log::debug!("{ui_filepath:?} parsed.");
+
+    log::debug!("building GTK UI...");
+    let builder = Builder::new();
+    builder
+        .add_from_string(&ui.gtk_ui)
+        .expect("Failed to build GTK UI");
+    log::debug!("GTK UI built.");
+
+    let app_window = gtk::ApplicationWindow::builder()
+        .application(app)
+        .child(&builder.object::<gtk::Widget>("body").unwrap())
+        .width_request(600)
+        .height_request(400)
+        .build();
+
+    app_window.show()
 }
 
 fn main() {
-    let state = Box::leak(Box::new(State::new()));
+    // Initialize logger.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let app = gtk::Application::builder()
         .application_id("dev.negrel.neww")
         .build();
 
-    app.connect_activate(|app| activate(app, state));
+    app.connect_activate(activate);
 
     app.run();
 }
