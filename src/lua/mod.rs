2cd36bd4e445437be8b6898c78d6b7c0c18f36bf
Alexandre Negrel <negrel.dev@protonmail.com>
2023-05-18 19:46:08 +0200

    add neww.ui.components and neww.ui modules

--- src/lua/mod.rs
+++ src/lua/mod.rs
@@ -1,7 +1,10 @@
 use anyhow::Context;
-use mlua::Lua;
+use mlua::{chunk, Lua, UserData};
 
-use self::{application::Application, bindings::gtk::Window};
+use self::{
+    application::Application,
+    bindings::gtk::{Button, Label, Window},
+};
 
 // Global application user data.
 pub mod application;
@@ -16,23 +19,62 @@ pub fn new_vm(app: Application) -> Result<Lua, anyhow::Error> {
         .set("application", app)
         .context("Failed to define global application table")?;
 
+    load_neww_ui_components_module(&vm).context("Failed to load neww.ui.components module")?;
     load_neww_ui_module(&vm).context("Failed to load neww.ui module")?;
 
     Ok(vm)
 }
 
-fn load_neww_ui_module(vm: &Lua) -> Result<(), anyhow::Error> {
+pub trait UIComponent: Default + UserData {}
+
+impl<T: Default + UIComponent> UIComponent for T {}
+
+fn load_neww_ui_components_module(vm: &Lua) -> Result<(), anyhow::Error> {
     let module = vm.create_function(|vm, ()| {
         let table = vm.create_table()?;
 
-        // Window constructor
-        table.set(
-            "window",
-            vm.create_function(|_vm, ()| Ok(Window(gtk::Window::new())))?,
-        )?;
+        macro_rules! component {
+            ($component_name:ident) => {
+                table.set(
+                    stringify!($component_name).to_lowercase(),
+                    vm.create_function(|_vm, ()| {
+                        Ok($component_name(gtk::$component_name::default()))
+                    })?,
+                )?;
+            };
+        }
+
+        component!(Window);
+        component!(Label);
+        component!(Button);
 
         Ok(table)
     })?;
+
+    vm.load_from_function::<_, mlua::Table>("neww.ui.components", module)?;
+
+    Ok(())
+}
+
+fn load_neww_ui_module(vm: &Lua) -> Result<(), anyhow::Error> {
+    let module = chunk! {
+        local components = require("neww.ui.components")
+        local M = {}
+        setmetatable(M, {
+            __index = function(table, key)
+                return function(attributes)
+                    local component = components[key]()
+                    // Set attributes
+                    for name, value in pairs(attributes) do
+                        component[name] = value
+                    end
+                    return component
+                end
+            end
+        })
+        return M
+    };
+    let module = vm.load(module).into_function()?;
 
     vm.load_from_function::<_, mlua::Table>("neww.ui", module)?;
 
