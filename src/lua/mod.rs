fe49af124addb25c1b03de553e3d90edf18821d0
Alexandre Negrel <negrel.dev@protonmail.com>
2023-06-15 23:38:04 +0200

    add lua timer module

--- src/lua/mod.rs
+++ src/lua/mod.rs
@@ -1,7 +1,9 @@
+use std::{cell::RefCell, rc::Rc};
+
 use anyhow::Context;
 use mlua::{chunk, Lua, UserData};
 
-use self::bindings::gtk::{Box, Button, Image, Label, Window};
+use self::bindings::gtk::{Box, Button, Image, Label, Scale, Window};
 
 // Bindings
 pub mod bindings;
@@ -16,6 +18,7 @@ pub fn new_vm(app: impl UserData + 'static) -> Result<&'static Lua, anyhow::Erro
 
     load_neww_ui_components_module(vm).context("Failed to load neww.ui.components module")?;
     load_neww_ui_module(vm).context("Failed to load neww.ui module")?;
+    load_neww_timer_module(vm).context("Failed to load neww.timer module")?;
 
     Ok(vm)
 }
@@ -44,6 +47,7 @@ fn load_neww_ui_components_module(vm: &Lua) -> Result<(), anyhow::Error> {
         component!(Button);
         component!(Image);
         component!(Box);
+        component!(Scale);
 
         Ok(table)
     })?;
@@ -74,6 +78,79 @@ fn load_neww_ui_module(vm: &Lua) -> Result<(), anyhow::Error> {
     let module = vm.load(module).into_function()?;
 
     vm.load_from_function::<_, mlua::Table>("neww.ui", module)?;
+
+    Ok(())
+}
+
+fn load_neww_timer_module(vm: &'static Lua) -> Result<(), anyhow::Error> {
+    let module = vm.create_function(|vm, ()| {
+        let table = vm.create_table()?;
+
+        let timers = Rc::new(RefCell::new(Vec::new()));
+
+        {
+            let timers = timers.clone();
+            table.set(
+                "set_interval",
+                vm.create_function(move |_vm, (callback, ms): (mlua::Function, u64)| {
+                    let source_id = gtk::glib::timeout_add_local(
+                        std::time::Duration::from_millis(ms),
+                        move || {
+                            let _ = callback.call::<_, ()>(());
+                            gtk::glib::Continue(true)
+                        },
+                    );
+
+                    timers.borrow_mut().push(source_id);
+
+                    Ok(timers.borrow().len() - 1)
+                })?,
+            )?;
+        }
+
+        {
+            let timers = timers.clone();
+            table.set(
+                "set_timeout",
+                vm.create_function(move |_vm, (callback, ms): (mlua::Function, u64)| {
+                    let source_id = gtk::glib::timeout_add_local(
+                        std::time::Duration::from_millis(ms),
+                        move || {
+                            let _ = callback.call::<_, ()>(());
+                            gtk::glib::Continue(false)
+                        },
+                    );
+
+                    timers.borrow_mut().push(source_id);
+
+                    Ok(timers.borrow().len() - 1)
+                })?,
+            )?;
+        }
+
+        {
+            table.set(
+                "clear_timer",
+                vm.create_function(move |_vm, index: mlua::Integer| {
+                    let index: usize =
+                        index
+                            .try_into()
+                            .map_err(|_err| mlua::Error::FromLuaConversionError {
+                                from: "integer",
+                                to: "usize",
+                                message: None,
+                            })?;
+                    let source_id: gtk::glib::SourceId = timers.borrow_mut().remove(index);
+                    source_id.remove();
+                    Ok(())
+                })?,
+            )?;
+        }
+
+        Ok(table)
+    })?;
+
+    vm.load_from_function::<_, mlua::Table>("neww.timer", module)?;
 
     Ok(())
 }
