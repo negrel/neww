840543138dc40ed345af1749c28b0f40f534fb45
Alexandre Negrel <negrel.dev@protonmail.com>
2023-11-21 23:31:09 +0100

    merge neww-gtk with neww

--- src/neww/gtk.lua
+++ src/neww/gtk.lua
@@ -3,43 +3,39 @@ local M = {}
 -- For GTK4 Layer Shell to get linked before libwayland-client we must explicitly load it before importing with lgi
 local ffi = require("ffi")
 ffi.cdef [[
-    void *dlopen(const char *filename, int flags);
-]]
+		void *dlopen(const char *filename, int flags);
+	]]
 ffi.C.dlopen("libgtk4-layer-shell.so", 0x101)
 
 local lgi = require("lgi")
-local Gtk = lgi.require("Gtk")
-
-local renderer = require("neww-gtk.renderer")
-
--- Define renderer.
-local neww = require("neww")
-neww.renderer = renderer
+M.layer_shell = lgi.require("Gtk4LayerShell")
+M.Gtk = lgi.require("Gtk")
+M.GLib = lgi.require("GLib")
 
 function M.create_app(app_props, window_props)
 	local caller_app_on_activate = app_props.on_activate
 	app_props.on_activate = nil
 
 	-- Create application.
-	local gtk_app = Gtk.Application(app_props)
+	local gtk_app = M.Gtk.Application(app_props)
 
 	return function(vnode)
 		gtk_app.on_activate = function()
 			window_props.application = gtk_app
-			local window = Gtk.Window(window_props)
+			local window = M.Gtk.Window(window_props)
 
 			if type(caller_app_on_activate) == "function" then
 				caller_app_on_activate(gtk_app, window)
 			end
 
-			local box = Gtk.Box {
+			local box = M.Gtk.Box {
 				homogeneous = true
 			}
 
 			window.child = box
 			window:show()
 
-			neww:render(vnode, box)
+			require("neww"):render(vnode, box)
 		end
 
 
